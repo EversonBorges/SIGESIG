be734b1f97eafd5bb4a2d0864c049092c453f7f2
everson <everson@borges-note>
2019-02-02 12:39:10 -0200

    commit

--- src/main/java/com/borges/igreja/WebSecurityConfig.java
+++ src/main/java/com/borges/igreja/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.
 			authorizeRequests()
-				//.antMatchers("/usuario/novoCadastro","/usuario/consultar").hasAnyRole("ADMIN")
+				.antMatchers("/usuario/novoCadastro","/usuario/consultar").hasAnyRole("ADMIN")
 				//.antMatchers("/financeiro").hasAnyRole("TESOURARIA")
 				//.antMatchers("/ministerio/novo","/membros","/visitantes/novo","/templos","/escolaBiblica").hasAnyRole("CADASTROUSUARIO")
 				//.antMatchers("/ministerio","/visitantes").hasAnyRole("ADMIN")
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,16 +1,15 @@
-INSERT INTO tb_usuario (ds_nome,ds_login,ds_senha,fl_ativo)
-  VALUES('Borges','borges','$2a$10$YYe9VtFGZoWvrNSZNV/AeuVSTOMQLxcGia4IQEl/yVaxrfAnPDcuO',true);
 
-INSERT INTO tb_grupo(ds_nome,ds_descricao) 
- VALUES('ADMINISTRADORES','Adminitrador');
+INSERT INTO tb_usuario (id_usuario,ds_nome,ds_login,ds_senha,fl_ativo)
+  VALUES(1,'Admin','admin','$2a$10$YYe9VtFGZoWvrNSZNV/AeuVSTOMQLxcGia4IQEl/yVaxrfAnPDcuO',true);
 
-INSERT INTO tb_permissao(ds_permissao,ds_descricao)
-   VALUES('ROLE_ADMIN','Adminitrador');
+INSERT INTO tb_grupo(id_grupo,ds_nome,ds_descricao)
+   VALUES(1,'ADM','Administrador');
 
-INSERT INTO tb_usuario_x_grupo(id_usuario,id_grupo)VALUES(2,1);
+INSERT INTO tb_permissao(id_permissao,ds_permissao,ds_descricao)
+   VALUES(1,'ROLE_ADMIN','Administrador');
 
-/*ROLE_CADASTROUSUARIO x BACKOFFICE*/
-INSERT INTO tb_permissao_x_grupo(id_permissao,id_grupo)VALUES(1,1);
-
-
+INSERT INTO tb_usuario_x_grupo(id_usuario,id_grupo)VALUES(1,1);
+ 
+ /*ROLE_CADASTROUSUARIO x BACKOFFICE*/
+INSERT INTO tb_permissao_x_grupo(id_permissao,id_grupo)VALUES(1,1); 
 
